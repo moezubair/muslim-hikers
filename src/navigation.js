6a83a954ee50de03c988462b039003a9a2d8317a
moezubair <mzubair01@gmail.com>
2024-01-03 21:10:01 -0500

    First commit

---
+++ src/navigation.js
@@ -0,0 +1,26 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+
+    {
+      text: 'Blog',
+      href: getBlogPermalink(),
+    },
+    // {
+    //   text: 'About us',
+    //   href: getPermalink('/about'),
+    // }
+    
+  ],
+  actions: [{ text: 'Join Us', href: 'https://fb.com/groups/muslim.hikers.ontario', target: '_blank' }],
+};
+
+export const footerData = {
+  socialLinks: [
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/muslim.hikers.ontario' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://fb.com/groups/muslim.hikers.ontario' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+  ],
+  footNote: ``
+};
