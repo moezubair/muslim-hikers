008f48e7c63152fb901cb0a9abf564a91861937c
moezubair <mzubair01@gmail.com>
2024-01-05 13:55:00 -0500

    Update main page, and add new jobs

--- src/navigation.js
+++ src/navigation.js
@@ -2,7 +2,10 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
-
+    {
+      text: 'Home',
+      href: getPermalink('/'),
+    },
     {
       text: 'Blog',
       href: getBlogPermalink(),
@@ -13,9 +16,23 @@ export const headerData = {
     },
     {
       text: 'Join the Team',
-      href: getPermalink('social-media'),
+      links:[
+        {
+          text: 'Social Media Coordinator',
+          href: getPermalink('/roles/social-media'),
+        },
+        {
+          text: 'Website Developer',
+          href: getPermalink('/roles/webdev'),
+        },
+      ]
     }
     
+  ],
+  socialLinks: [
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/muslim.hikers.ontario' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://fb.com/groups/muslim.hikers.ontario' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   actions: [{ text: 'Events', href: 'https://fb.com/groups/muslim.hikers.ontario', target: '_blank' }],
 };
